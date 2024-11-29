60b67742d88d7eb7ee9c5fe9c4dc8aec970e68bd
NicholasHG25 <nicholashg0248@gmail.com>
2024-11-29 19:59:40 +0700

    membuat search dan dapat mengarah ke movie details

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,260 +1,98 @@
-import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'movie_screen.dart'; // Sesuaikan path dengan struktur proyek Anda
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
+  const SearchScreen({Key? key}) : super(key: key);
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
+  _SearchScreenState createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  String _searchText = '';
-  List _movies = [];
-  List _popularMovies = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchPopularMovies();
-  }
-
-  Future<void> _fetchPopularMovies() async {
-    const String apiKey = '38044b25292884813a6d0f737734e700';
-    final url =
-        'https://api.themoviedb.org/3/movie/popular?api_key=$apiKey&language=en-US&page=1';
-
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        setState(() {
-          _popularMovies = data['results'] ?? [];
-        });
-      } else {
-        throw Exception('Failed to load popular movies');
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
-
-  Future<void> _searchMovies(String query) async {
-    if (query.isEmpty) {
-      setState(() {
-        _movies = [];
-        _searchText = query;
-      });
-      return;
-    }
-
-    const String apiKey = '38044b25292884813a6d0f737734e700';
-    final url =
-        'https://api.themoviedb.org/3/search/movie?api_key=$apiKey&query=$query';
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        setState(() {
-          _movies = data['results'] ?? [];
-          _searchText = query;
-        });
-      } else {
-        throw Exception('Failed to load movies');
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
-
-  void _navigateToDetails(BuildContext context, Map movie) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => MovieDetailsScreen(movie: movie),
-      ),
-    );
-  }
+  final TextEditingController _searchController = TextEditingController();
+  String _query = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[900],
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: TextField(
-            onChanged: (value) => _searchMovies(value),
-            style: const TextStyle(color: Colors.white),
-            decoration: const InputDecoration(
-              hintText: 'Search for movies...',
-              hintStyle: TextStyle(color: Colors.grey),
-              border: InputBorder.none,
-              prefixIcon: Icon(Icons.search, color: Colors.grey),
-            ),
+        title: TextField(
+          controller: _searchController,
+          onChanged: (value) {
+            setState(() {
+              _query = value.toLowerCase();
+            });
+          },
+          style: const TextStyle(color: Colors.white),
+          decoration: const InputDecoration(
+            hintText: "Search for movies...",
+            hintStyle: TextStyle(color: Colors.grey),
+            border: InputBorder.none,
           ),
         ),
       ),
-      body: Container(
-        color: Colors.black,
-        child: _searchText.isEmpty
-            ? _buildMovieList(_popularMovies)
-            : _buildMovieList(_movies),
-      ),
-    );
-  }
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('film')
+            .snapshots(), // Ambil semua data film
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No movies found.'));
+          }
 
-  Widget _buildMovieList(List movies) {
-    return ListView.builder(
-      itemCount: movies.length,
-      itemBuilder: (context, index) {
-        final movie = movies[index];
-        final posterPath = movie['poster_path'];
-        final title = movie['title'] ?? 'No title';
-        final releaseDate = movie['release_date'] ?? 'Unknown';
-        final posterUrl = posterPath != null
-            ? 'https://image.tmdb.org/t/p/w200$posterPath'
-            : null;
+          // Filter berdasarkan query
+          final filteredMovies = snapshot.data!.docs.where((doc) {
+            final title = (doc['title'] ?? '').toString().toLowerCase();
+            return title.contains(_query);
+          }).toList();
 
-        return GestureDetector(
-          onTap: () => _navigateToDetails(context, movie),
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[850],
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    posterUrl != null
-                        ? ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.network(
-                              posterUrl,
-                              height: 120,
-                              width: 80,
-                              fit: BoxFit.cover,
-                            ),
-                          )
-                        : Container(
-                            height: 120,
-                            width: 80,
-                            color: Colors.grey,
-                            child: const Icon(
-                              Icons.movie,
-                              color: Colors.white,
-                              size: 50,
-                            ),
-                          ),
-                    const SizedBox(width: 16.0),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            title,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            'Release Date: $releaseDate',
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 14.0,
-                            ),
-                          ),
-                        ],
+          if (filteredMovies.isEmpty) {
+            return const Center(child: Text('No results found.'));
+          }
+
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 0.7,
+            ),
+            itemCount: filteredMovies.length,
+            itemBuilder: (context, index) {
+              final movie =
+                  filteredMovies[index].data() as Map<String, dynamic>;
+
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MovieScreen(
+                        documentId: filteredMovies[index].id,
                       ),
                     ),
-                  ],
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(movie['image'] ?? ''),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class MovieDetailsScreen extends StatelessWidget {
-  final Map movie;
-
-  const MovieDetailsScreen({required this.movie, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final posterPath = movie['poster_path'];
-    final posterUrl = posterPath != null
-        ? 'https://image.tmdb.org/t/p/w500$posterPath'
-        : null;
-    final title = movie['title'] ?? 'No title';
-    final overview = movie['overview'] ?? 'No description available';
-    final releaseDate = movie['release_date'] ?? 'Unknown';
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        backgroundColor: Colors.black,
+              );
+            },
+          );
+        },
       ),
       backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              posterUrl != null
-                  ? Center(
-                      child: Image.network(posterUrl),
-                    )
-                  : Container(
-                      height: 300,
-                      color: Colors.grey,
-                      child: const Icon(
-                        Icons.movie,
-                        color: Colors.white,
-                        size: 100,
-                      ),
-                    ),
-              const SizedBox(height: 16.0),
-              Text(
-                title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                'Release Date: $releaseDate',
-                style: const TextStyle(color: Colors.grey, fontSize: 16.0),
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                overview,
-                style: const TextStyle(color: Colors.white, fontSize: 16.0),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
