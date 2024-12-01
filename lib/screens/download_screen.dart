2c37a9ad2c722563d1a888ac337b297dc3394c74
yumnaaprilia <yumna.535230179@stu.untar.ac.id>
2024-12-01 17:21:15 +0700

    membuat layar menampilkan dan mengelola daftar film yang diunduh menggunakan data dari firebase firestore

--- lib/screens/download_screen.dart
+++ lib/screens/download_screen.dart
@@ -1,187 +1,174 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:netflix_clone/screens/globals.dart' as globals;
+import 'dart:math'; // Tambahkan import ini untuk menghasilkan angka acak
+
 
 class DownloadScreen extends StatefulWidget {
-  const DownloadScreen({super.key});
+  const DownloadScreen({Key? key}) : super(key: key);
 
   @override
   _DownloadScreenState createState() => _DownloadScreenState();
 }
 
+// Fungsi untuk menghasilkan durasi acak
+String _generateRandomDuration() {
+  final random = Random();
+  final hours = random.nextInt(3) + 1; // Antara 1 hingga 3 jam
+  final minutes = random.nextInt(60); // Antara 0 hingga 59 menit
+  return '${hours}h ${minutes}m';
+}
+
 class _DownloadScreenState extends State<DownloadScreen> {
-  final List<Map<String, dynamic>> episodes = [
-    {
-      'title': 'Season 1: Pilot',
-      'duration': '54m',
-      'description':
-          'After a hurricane, John B, JJ, Pope and Kiara plunge headlong into danger and adventure when they find a mysterious sunken wreck.',
-      'image': 'assets/episode1.jpg',
-      'isDownloading': false,
-      'progress': 0.0,
-      'isDownloaded': false,
-    },
-    {
-      'title': 'Episode 2: The Weirdo on Maple Street',
-      'duration': '55m',
-      'description':
-          'Lucas, Mike and Dustin try to talk to the girl they found in the woods...',
-      'image': 'assets/episode2.jpg',
-      'isDownloading': false,
-      'progress': 0.0,
-      'isDownloaded': false,
-    },
-    {
-      'title': 'Episode 3: Holly, Jolly',
-      'duration': '51m',
-      'description':
-          'An increasingly concerned Nancy looks for Barb and finds out what Jonathan\'s been up to...',
-      'image': 'assets/episode3.jpg',
-      'isDownloading': false,
-      'progress': 0.0,
-      'isDownloaded': false,
-    },
-  ];
+  List<Map<String, dynamic>> downloadedMovies = [];
 
-  void _startDownload(int index) {
-    setState(() {
-      episodes[index]['isDownloading'] = true;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _fetchDownloads();
+  }
 
-    Future.delayed(const Duration(seconds: 5), () {
-      setState(() {
-        episodes[index]['progress'] = 1.0;
-        episodes[index]['isDownloading'] = false;
-        episodes[index]['isDownloaded'] = true;
-      });
-    });
+  Future<void> _fetchDownloads() async {
+    final docRef = FirebaseFirestore.instance
+        .collection('downloadedMovies')
+        .doc(globals.uid);
 
-    for (int i = 1; i <= 10; i++) {
-      Future.delayed(Duration(milliseconds: i * 500), () {
+    try {
+      final snapshot = await docRef.get();
+      if (snapshot.exists) {
+        final data = snapshot.data() as Map<String, dynamic>;
         setState(() {
-          episodes[index]['progress'] = i / 10;
+          downloadedMovies = data.entries
+              .map((entry) => {
+                    'movieId': entry.key,
+                    ...entry.value as Map<String, dynamic>
+                  })
+              .toList();
         });
-      });
+      }
+    } catch (e) {
+      debugPrint("Error fetching downloads: $e");
     }
   }
 
-  void _deleteDownload(int index) {
-    setState(() {
-      episodes[index]['progress'] = 0.0;
-      episodes[index]['isDownloaded'] = false;
-    });
+  Future<void> _deleteDownload(String movieId) async {
+    final docRef = FirebaseFirestore.instance
+        .collection('downloadedMovies')
+        .doc(globals.uid);
+
+    try {
+      await docRef.update({movieId: FieldValue.delete()});
+      setState(() {
+        downloadedMovies.removeWhere((movie) => movie['movieId'] == movieId);
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Download removed successfully.')),
+      );
+    } catch (e) {
+      debugPrint("Error deleting download: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to remove download.')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.pop(context),
-        ),
         title: const Text(
           'My Downloads',
           style: TextStyle(color: Colors.white),
         ),
       ),
       backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-      body: ListView.builder(
-        itemCount: episodes.length,
-        itemBuilder: (context, index) {
-          final episode = episodes[index];
-          return Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      body: downloadedMovies.isEmpty
+          ? const Center(
+              child: Text(
+                'No downloads available.',
+                style: TextStyle(color: Colors.grey),
+              ),
+            )
+          : ListView.builder(
+              itemCount: downloadedMovies.length,
+              itemBuilder: (context, index) {
+                final movie = downloadedMovies[index];
+                              final duration = movie['duration'] ?? _generateRandomDuration();
+
+                return Column(
                   children: [
-                    // Gambar episode
-                    Container(
-                      width: 120.0,
-                      height: 80.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: AssetImage(episode['image']),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 16.0),
-                    // Detail episode
-                    Expanded(
-                      child: Column(
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 16.0),
+                      child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            episode['title'],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
+                          // Gambar film
+                          Container(
+                            width: 120.0,
+                            height: 80.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              image: DecorationImage(
+                                image: NetworkImage(movie['image'] ?? ''),
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
-                          const SizedBox(height: 4.0),
-                          Text(
-                            episode['duration'],
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 14.0,
+                          const SizedBox(width: 16.0),
+                          // Detail film
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  movie['title'] ?? 'No Title',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 4.0),
+                                Text(
+                                  duration,
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14.0,
+                                  ),
+                                ),
+                                const SizedBox(height: 8.0),
+                                Text(
+                                  movie['deskripsi'] ?? '',
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 12.0,
+                                  ),
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
                             ),
                           ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            episode['description'],
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12.0,
-                            ),
-                            maxLines: 3, // Maksimal 3 baris untuk deskripsi
-                            overflow: TextOverflow
-                                .ellipsis, // Tambahkan ellipsis jika teks terlalu panjang
+                          const SizedBox(width: 16.0),
+                          // Tombol hapus
+                          IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () => _deleteDownload(movie['movieId']),
                           ),
-                          const SizedBox(height: 8.0),
-                          if (episode['isDownloading'])
-                            LinearProgressIndicator(
-                              value: episode['progress'],
-                              backgroundColor: Colors.grey[800],
-                              color: Colors.red,
-                            ),
                         ],
                       ),
                     ),
-                    const SizedBox(width: 16.0),
-                    // Tombol aksi
-                    episode['isDownloaded']
-                        ? IconButton(
-                            icon: const Icon(Icons.delete, color: Colors.red),
-                            onPressed: () => _deleteDownload(index),
-                          )
-                        : IconButton(
-                            icon: Icon(
-                              episode['isDownloading']
-                                  ? Icons.downloading
-                                  : Icons.download,
-                              color: Colors.white,
-                            ),
-                            onPressed: episode['isDownloading']
-                                ? null
-                                : () => _startDownload(index),
-                          ),
+                    const Divider(
+                      color: Colors.grey,
+                      height: 1.0,
+                      indent: 16.0,
+                      endIndent: 16.0,
+                    ),
                   ],
-                ),
-              ),
-              const Divider(
-                color: Colors.grey,
-                height: 1.0,
-                indent: 16.0,
-                endIndent: 16.0,
-              ),
-            ],
-          );
-        },
-      ),
+                );
+              },
+            ),
     );
   }
 }
